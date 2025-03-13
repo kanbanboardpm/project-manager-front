4431ce8ad3e0c5be06b2aa0a203894069b3c7377
Lee jihun <sawoo0999@naver.com>
2025-03-13 18:41:55 +0900

    style: 댓글창 스타일 수정

--- src/components/card/meta/CommentSection.tsx
+++ src/components/card/meta/CommentSection.tsx
@@ -105,7 +105,7 @@ export default function CommentSection({
       </div>
 
       {/* 댓글 목록 */}
-      <div className="max-h-[190px] overflow-auto space-y-4">
+      <div className="md:max-h-[110px]  max-h-[600px] overflow-auto space-y-4">
         {comments?.map((comment) => {
           const isEditing = editingCommentId === comment.commentId
           const isValid = loggedInUser.nickName === comment.nickName
